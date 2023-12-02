b9b4e3f2dc5b5e6ba4764d679d026b4a1375fd54
golu7679 <golurajak811@gmail.com>
2023-12-02 09:48:32 +0530

    basic setup

---
+++ default-tsconfig/index.ts
@@ -0,0 +1,58 @@
+import 'dotenv/config';
+
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import { Express, NextFunction, Response } from 'express';
+import * as cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan'
+
+const app: Express = express();
+const PORT = +process.env.PORT || 3001;
+
+// Middleware
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(
+    cors({
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }),
+);
+morgan(':method :url :status :res[content-length] - :response-time ms')
+// security
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                'script-src': ["'self'",],
+                'img-src': ["'self'",],
+            },
+        },
+        crossOriginResourcePolicy: false,
+    }),
+);
+
+app.use((_, res: Response, next: NextFunction) => {
+    res.setHeader('Permissions-Policy', '');
+    next();
+});
+// app.set('trust proxy', 1); // for session
+
+// to access files
+app.use(express.static('public'));
+app.use('/assets', express.static('assets'));
+app.use('/.well-known', express.static('.well-known'));
+app.use('/favicon.ico', express.static('favicon.ico'));
+
+app.set('view engine', 'ejs');
+
+app.disable('x-powered-by');
+
+// database operation logs
+// mongoose.set('debug', true);
+
+
+// Server Listing At
+app.listen(PORT, async () => {
+    console.log(`Server is Running at http://localhost:` + PORT);
+});
